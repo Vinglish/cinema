1183acc4f99e6fdcc1283f1770341d0c20fa58ff
Vinglish <khan.ildar94@gmail.com>
2020-05-20 13:01:16 +0300

    fix transactions

--- src/main/java/com/dev/cinema/model/Movie.java
+++ src/main/java/com/dev/cinema/model/Movie.java
@@ -3,15 +3,11 @@ package com.dev.cinema.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
-@ToString(of = {"title", "description"})
+@ToString
 @Entity
 @Table(name = "movies")
 public class Movie {
