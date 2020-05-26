1b8f02e3d6a2bce7d27c00a22232b5ec29e54d1a
Vinglish <khan.ildar94@gmail.com>
2020-05-26 08:18:50 +0300

    fixes1

--- src/main/java/com/dev/cinema/model/User.java
+++ src/main/java/com/dev/cinema/model/User.java
@@ -6,8 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
