4e36a16520d6f861aece1bc688fd30a54c026fb0
Vinglish <khan.ildar94@gmail.com>
2020-06-16 13:21:56 +0300

    delete @NotNull

--- src/main/java/com/dev/cinema/models/dto/cinemahall/CinemaHallRequestCreateDto.java
+++ src/main/java/com/dev/cinema/models/dto/cinemahall/CinemaHallRequestCreateDto.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 
 @Getter
 public class CinemaHallRequestCreateDto {
-    @NotNull(message = "Capacity can't be empty!")
+
     private int capacity;
+
     @NotNull(message = "Can't be null!")
     @Size(min = 6, max = 150, message = "Number of symbols must be between 6 and 150!")
     private String description;
